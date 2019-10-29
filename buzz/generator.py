d1fef4ae0b4d566ff4a6fc713b5243ccd8436ed5
cbamend93 <brown.a.caleb93@gmail.com>
2019-10-29 04:30:47 +0000

    changed functionality

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('A01023052', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
-adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adjectives = ('Caleb Brown', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
